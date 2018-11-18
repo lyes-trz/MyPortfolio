682ef2248a007c762eb8cc5403ce9daed7cac408
ilyas jabar <ilyasjabar@gmail.com>
2018-11-18 17:26:32 +0100

    V1

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+
+const app = express()
+
+
+app.use('/', serveStatic(path.join(__dirname,'/dist')))
+
+const port = process.env.PORT || 8080
+app.listen(port)
+
+console.log('listening on port:' + port)
